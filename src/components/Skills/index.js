00677d6235a2de49fa94fbbd7d333f1ba174775f
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-05-14 06:05:32 -0300

    Fixed: attribute className in Skills component

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -8,47 +8,47 @@ export default function Skills() {
         Skills
       </h3>
       <div className="container">
-        <div class="row my-4">
-          <div class="col-sm">
+        <div className="row my-4">
+          <div className="col-sm">
             <p className="h4">Html 5</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">Css 3</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">Javascript</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">Java</p>
           </div>
         </div>
 
-        <div class="row my-4">
-          <div class="col-sm">
+        <div className="row my-4">
+          <div className="col-sm">
             <p className="h4">NodeJs/ExpressJs</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">ReactJs</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">NextJs</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">Bootstrap - Chakra UI</p>
           </div>
         </div>
 
-        <div class="row my-4">
-          <div class="col-sm">
+        <div className="row my-4">
+          <div className="col-sm">
             <p className="h4">MySQL</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">PostgreSQL</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">MongoDB</p>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
             <p className="h4">Linux</p>
           </div>
         </div>
