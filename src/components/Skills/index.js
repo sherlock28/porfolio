2b724a8fc396ba500dbef8603095456ff9daf4e1
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-05-15 21:28:46 -0300

    Using Grid for the Skills component

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./Skills.module.css";
+import { AiFillHtml5 } from 'react-icons/ai';
+import { DiCss3 } from "react-icons/di";
+import { SiJavascript, SiChakraui, SiCsharp, SiMysql, SiPostgresql, SiMongodb } from "react-icons/si";
+import { FaNodeJs, FaReact, FaVuejs, FaBootstrap } from "react-icons/fa";
 
 export default function Skills() {
   return (
@@ -8,49 +12,19 @@ export default function Skills() {
         Skills
       </h3>
       <div className="container">
-        <div className="row my-4">
-          <div className="col-sm">
-            <p className="h4">Html 5</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">Css 3</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">Javascript</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">Java</p>
-          </div>
-        </div>
-
-        <div className="row my-4">
-          <div className="col-sm">
-            <p className="h4">NodeJs/ExpressJs</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">ReactJs</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">NextJs</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">Bootstrap - Chakra UI</p>
-          </div>
-        </div>
-
-        <div className="row my-4">
-          <div className="col-sm">
-            <p className="h4">MySQL</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">PostgreSQL</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">MongoDB</p>
-          </div>
-          <div className="col-sm">
-            <p className="h4">Linux</p>
-          </div>
+        <div className="row row-cols-4">
+          <div className="col"><i className="h1"><AiFillHtml5 /></i></div>
+          <div className="col"><i className="h1"><DiCss3 /></i></div>
+          <div className="col"><i className="h1"><SiJavascript /></i></div>
+          <div className="col"><i className="h1"><FaNodeJs /></i></div>
+          <div className="col"><i className="h1"><FaReact /></i></div>
+          <div className="col"><i className="h1"><FaVuejs /></i></div>
+          <div className="col"><i className="h1"><FaBootstrap /></i></div>
+          <div className="col"><i className="h1"><SiChakraui /></i></div>
+          <div className="col"><i className="h1"><SiCsharp /></i></div>
+          <div className="col"><i className="h1"><SiMysql/></i></div>
+          <div className="col"><i className="h1"><SiPostgresql /></i></div>
+          <div className="col"><i className="h1"><SiMongodb /></i></div>
         </div>
       </div>
     </div>
